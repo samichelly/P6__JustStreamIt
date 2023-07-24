bfcda363206f887a752b16e5c391916a59471078
Sami Chelly <samichelly15@gmail.com>
2023-07-24 21:01:49 +0200

    suppresion du HTML en dur pour les 3 catégories au choix, remplacer par la création dynamique des catégories depuis le JS + Ajout d'une boucle dans la fonction movies_by_category pour fetch autant de fois que nécessaire en fonction de la limite fixé + POPUP : séparation de la requete API et de la création de la popup

--- JustStreamIt/scripts/main.js
+++ JustStreamIt/scripts/main.js
@@ -1,22 +1,29 @@
 const url = 'http://localhost:8000/api/v1/titles/';
 
-// récupérer data best_movie
-function get_main_movie(imdb_sorting = "-imdb_score", limit = 1) {
-    fetch(url + "?sort_by=" + imdb_sorting + "&limit=" + limit)
+
+// séparer l'obtention du dictionnaire et la l'affichage à partir du bouton
+// Récupérer data best_movie
+function get_main_movie(imdb_sorting = "-imdb_score") {
+    fetch(url + "?sort_by=" + imdb_sorting)
         .then(function (response) {
             if (response.ok) {
                 return response.json();
             } else {
-                throw new Error('Echec : ' + response.status);
+                console.error("Page non atteinte, Erreur :", response.status);
             }
         })
         .then(function (data) {
             let bestMovie = data.results[0]
-            document.getElementById('best_movie').innerHTML = 
-            `<h2>${bestMovie.title} (${bestMovie.year})</h2>
+            document.getElementById('best_movie').innerHTML =
+                `<h2>${bestMovie.title} (${bestMovie.year})</h2>
             <img src=  ${bestMovie.image_url}>`;
             const bestButton = document.getElementById('btn_best_movie');
             bestButton.addEventListener('click', () => showMoviePopup(bestMovie.id));
+            console.log("test")
+            console.log(data.results.slice(1, 8))
+            // return data.results.slice(1, 3)
+            
+            // Récupérer les data.results[1:8] pour gérer la partie Meilleurs Films
         })
         .catch(function (error) {
             console.log(error);
@@ -24,64 +31,45 @@ function get_main_movie(imdb_sorting = "-imdb_score", limit = 1) {
 }
 
 
-
+// ajouter l'argument les meilleurs films
+// genre facultatif
+// evolution possible, penser avec une fonction pour définir les paramèters et arguments
 async function movies_by_category(genre, limit = 7) {
-    const movies = await fetchMoviesByPage(genre, 1);
-    const moviesPage2 = await fetchMoviesByPage(genre, 2);
-    const allMovies = movies.concat(moviesPage2);
+    const pagesToFetch = Math.ceil(limit / 5);
+    const allMovies = [];
+    for (let page = 1; page <= pagesToFetch; page++) {
+        const moviesPage = await fetchMoviesByPage(genre, page);
+        allMovies.push(...moviesPage);
+    }
     if (allMovies.length > limit) {
         allMovies.splice(limit);
     }
     return allMovies;
 }
 
+
 async function fetchMoviesByPage(genre, page) {
-    const query_parameters = `?genre=${genre}&page=${page}`;
+    console.log("hhhhh")
+    const query_parameters = `?genre=${genre}&sort_by=-imdb_score&page=${page}`;
+    console.log("qqqqqq")
     const url_category = url + query_parameters;
     const response = await fetch(url_category);
     const data = await response.json();
-    const movies = data.results;
-    return movies;
+    return data.results;
 }
 
 
-function carrousel(movies, genre) {
-    const swiperWrapper = document.querySelector(`.categorie .swiper-wrapper`);
 
-    movies.forEach(movie => {
-        const movieCard = document.createElement('div');
-        movieCard.classList.add('swiper-slide');
-        movieCard.innerHTML = `
-          <h2>${movie.title}</h2>
-          <img src="${movie.image_url}" alt="${movie.title}" />
-          <button class="btn" data-movie-id="${movie.id}">More details</button>`;
-        swiperWrapper.appendChild(movieCard);
-    });
-
-    // Initialiser le carrousel avec Swiper
-    new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        spaceBetween: 10,
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 4,
-            },
-        },
-    });
-    document.createElement('h2').innerHTML = genre;
-    const buttons = document.querySelectorAll(`.categorie .btn`);
-    buttons.forEach(button => {
-        button.addEventListener('click', () => showMoviePopup(button.dataset.movieId));
-    });
+// POPUP //
+async function fetchMovieDetails(movieId) {
+    try {
+        const response = await fetch(url + movieId);
+        return await response.json();
+    } catch (error) {
+        console.error("Erreur lors de la récupération des détails du film :", error);
+    }
 }
 
-
-// Fonction pour créer et afficher la popup
 function showMoviePopup(movieId) {
     const popup = document.createElement('div');
     popup.classList.add('popup');
@@ -93,13 +81,11 @@ function showMoviePopup(movieId) {
     closeButton.id = 'close-popup-button';
     closeButton.textContent = 'Fermer';
 
-    // Récupérer les détails du film depuis l'API
-    fetch(url + movieId)
-        .then(response => response.json())
+    fetchMovieDetails(movieId)
         .then(data => {
             popupContent.innerHTML = `
                 <h2>${data.title} (${data.year})</h2>
-                <img src= ${data.image_url}>
+                <img src="${data.image_url}" alt="${data.title}" />
                 <p><strong>Genres:</strong> ${data.genres.join(', ')}</p>
                 <p><strong>Description:</strong> ${data.description}</p>
                 <p><strong>IMDb Score:</strong> ${data.imdb_score}</p>
@@ -109,13 +95,10 @@ function showMoviePopup(movieId) {
                 <p><strong>Countries:</strong> ${data.countries.join(', ')}</p>
                 <p><strong>Languages:</strong> ${data.languages.join(', ')}</p>
             `;
-
-            // Ajouter les éléments au DOM
             popup.appendChild(popupContent);
             popup.appendChild(closeButton);
             document.body.appendChild(popup);
 
-            // Gérer l'événement du bouton "Fermer"
             closeButton.addEventListener('click', () => {
                 popup.remove();
             });
@@ -125,12 +108,70 @@ function showMoviePopup(movieId) {
         });
 }
 
+
+// CAROUSSEL //
+function carrousel(movies, genre) {
+    const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('swiper-slide');
+        movieCard.innerHTML = `
+          <h2>${movie.title}</h2>
+          <img src="${movie.image_url}" alt="${movie.title}" />
+          <button class="btn" data-movie-id="${movie.id}">More details</button>`;
+        swiperWrapper.appendChild(movieCard);
+    });
+
+    const carrouselContainer = document.createElement('div');
+    carrouselContainer.classList.add('swiper-container');
+    carrouselContainer.appendChild(swiperWrapper);
+
+    const nextButton = document.createElement('div');
+    nextButton.classList.add('swiper-button-next');
+    const prevButton = document.createElement('div');
+    prevButton.classList.add('swiper-button-prev');
+
+    carrouselContainer.appendChild(nextButton);
+    carrouselContainer.appendChild(prevButton);
+
+    document.querySelector(`.categorie.${genre}`).appendChild(carrouselContainer);
+
+    // Initialiser le carrousel avec Swiper
+    new Swiper(carrouselContainer, {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: nextButton,
+            prevEl: prevButton,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 4,
+            },
+        },
+    });
+
+    const buttons = document.querySelectorAll(`.categorie.${genre} .btn`);
+    buttons.forEach(button => {
+        button.addEventListener('click', () => showMoviePopup(button.dataset.movieId));
+    });
+}
+
 async function main() {
     try {
         const mainMovies = get_main_movie();
-        const genres = ["Adventure"];//, "Drama", "Fantasy"];
+        const genres = ["Adventure", "Drama", "Fantasy"];
         for (const genre of genres) {
             const movies = await movies_by_category(genre);
+            const section = document.createElement('section');
+            section.classList.add('categorie', genre);
+            const h1 = document.createElement('h1');
+            h1.textContent = genre;
+            section.appendChild(h1);
+            document.querySelector('main').appendChild(section);
             carrousel(movies, genre);
             console.log(genre);
         }
